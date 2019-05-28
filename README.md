4456bbf960e4a19341dd0d55e314d9f6fd5c0fb1
Chris Michael <chris.perez@upr.edu>
2019-05-28 19:32:51 -0400

    :sparkles: [Vuejs/Vuex] Template Starter, first commmit.

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-# vuex-store-starter
+# Template Starter | vuex-store-starter
+💡 *This is a vue/vuex template starter that structures our store in a simple and efficient way.* 
 
 ## Project setup
 ```
@@ -30,5 +31,22 @@ npm run lint
 npm run test:unit
 ```
 
+## Dependencies
+- "axios": "^0.18.0",
+- "core-js": "^2.6.5",
+- "lodash": "^4.17.11",
+- "vue": "^2.6.10",
+- "vue-router": "^3.0.3",
+- "vuex": "^3.0.1"
+
+
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+
+## Where to get Help
+If you have any questions about the template, contact me at email chrisperezsantiago1@gmail.com
+For more details on how the template works, then check the article [Structuring store with the right feet using vue /vuex](https://medium.com/@chrismichaelperez/structuring-store-with-the-right-feet-using-vue-vuex-4ab69070cac0)
+
+
+I'll be glad you give this template a good use! 💖
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,46 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <HelloWorld msg="Welcome to Your Vue.js App + Store Structured"/>
+    <br>
+    <h2>👑Example request using JSONPlaceholder👑</h2>
+    <input type="text" v-model="search" placeholder="search ...">
+    <div>
+      <div v-if="isLoading">
+        Loading ...
+      </div>
+      <div v-else>
+        <div v-for="content in filteredDataObj" :key="content.id">
+          <li>{{content.title}}</li>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+  import {mapState , mapGetters} from 'vuex';
+  import HelloWorld from "@/components/HelloWorld.vue";
+  import store from '../store/store.js';
 
-export default {
-  name: "home",
-  components: {
-    HelloWorld
-  }
-};
+  export default {
+    name: "home",
+    components: {HelloWorld},
+    data(){
+      return{
+        search: ''
+      }
+    },
+    computed:{
+      ...mapState(['data' , 'isLoading']),
+      ...mapGetters(['FILTER_SPESIFIC_DATA']),
+      filteredDataObj(){
+        return this.FILTER_SPESIFIC_DATA(this.search)
+      }
+    },
+    created(){
+      store.dispatch('GET_DATA')
+    }
+  };
 </script>
+
